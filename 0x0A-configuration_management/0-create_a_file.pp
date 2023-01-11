dec1834df8ae7ca85d5e354b84a4815647dd9e81
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2023-01-11 08:46:49 +0100

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-#Creates /tmp/holberton with the given attributes
-file { '/tmp/holberton':
+#Creates /tmp/school with the given attributes
+file { '/tmp/school':
   owner   => www-data,
   group   => www-data,
   mode    => '0744',
